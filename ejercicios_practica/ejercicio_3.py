a59793c3351190742b65393e82e87307ad213c34
Mariano Andolina <mariano.3gs@gmail.com>
2022-10-13 20:00:37 -0300

    2st commit ejercicios 13/10

--- ejercicios_practica/ejercicio_3.py
+++ ejercicios_practica/ejercicio_3.py
@@ -11,8 +11,9 @@
 
 # --------------------------------
 # Aquí dentro definir la función ordenar
-def ordenar(lista):
-
+def ordenar(numeros):
+    numeros_ordenados = sorted(numeros)
+    return numeros_ordenados
 # --------------------------------
 
 
@@ -33,9 +34,11 @@ if __name__ == '__main__':
 
     # Luego de crear la función invocarla en este lugar:
 
-    # lista_ordenada = ordenar(numeros)
+    lista_ordenada = ordenar(numeros)
 
     # Imprimir en pantalla "lista_ordenada" que tendrá
     # los valores retornado por la función ordenar:
+
+    print('La lista de numeros ordenada queda asi: ', lista_ordenada)
 
     print("terminamos")
